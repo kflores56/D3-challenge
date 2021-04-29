66895a1c8d953446e95a17aead85dacf65bad076
Kristin Flores <kristin@Joan-Steppenbeck.local>
2021-04-29 16:21:02 -0500

    second x axis

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,20 +24,58 @@ var svg = d3.select("#scatter")
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Import Data
+// Initial Params 
+var chosenXAxis = "poverty";
+
+// function for updating x-scale with selected
+function xScale(demoData, chosenXAxis) {
+  var xLinearScale = d3.scaleLinear()
+  .domain([d3.min(demoData, d => d[chosenXAxis]) * 0.9,
+    d3.max(demoData, d => d[chosenXAxis]) * 1.05
+])
+  .range([0, width]);
+
+  return xLinearScale
+}
+
+// function used for updating x axis when selected 
+function renderAxes(newXScale, xAxis) {
+  var bottomAxis = d3.axisBottom(newXScale);
+
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+
+  return xAxis;
+}
+
+// function for updating circle group with a transition 
+function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cx", d => newXScale(d[chosenXAxis]));
+
+  return circlesGroup;
+}
+
+  // Import Data
 d3.csv("./assets/data/data.csv").then(function(demoData) {
 
   // Parse Data
   demoData.forEach(function(data) {
     data.poverty = +data.poverty; 
     data.healthcare = +data.healthcare;
-    // data.abbr = +data.abbr;
+    data.age = +data.age;
   });
 
-    // Create scale functions
-    var xLinearScale = d3.scaleLinear()
-      .domain([8.5, d3.max(demoData, d => d.poverty)])
-      .range([0, width]);
+    // // Create scale functions // ONE AXIS
+    // var xLinearScale = d3.scaleLinear()
+    //   .domain([8.5, d3.max(demoData, d => d.poverty)])
+    //   .range([0, width]);
+
+    // xLinearScale function above csv import
+    var xLinearScale = xScale(demoData, chosenXAxis);
 
     var yLinearScale = d3.scaleLinear()
       .domain([0, d3.max(demoData, d => d.healthcare)])
@@ -48,7 +86,7 @@ d3.csv("./assets/data/data.csv").then(function(demoData) {
     var leftAxis = d3.axisLeft(yLinearScale);
 
      // Append Axes to the chart
-    chartGroup.append("g")
+    var xAxis = chartGroup.append("g")
       .attr("transform", `translate(0, ${height})`)
       .call(bottomAxis);
 
@@ -56,7 +94,7 @@ d3.csv("./assets/data/data.csv").then(function(demoData) {
       .call(leftAxis);
 
     // Create Circles
-    chartGroup.selectAll("circle")
+    var circlesGroup = chartGroup.selectAll("circle")
       .data(demoData)
       .enter()
       .append("circle")
@@ -66,8 +104,8 @@ d3.csv("./assets/data/data.csv").then(function(demoData) {
       .classed("stateCircle", true)
       .attr("opacity", ".5");
 
-    // Create labels for circles
-    chartGroup.selectAll(null)
+    // Create labels for circles // POVERTY
+    var pov_abbr = chartGroup.selectAll(null)
       .data(demoData)
       .enter()
       .append("text")
@@ -75,8 +113,44 @@ d3.csv("./assets/data/data.csv").then(function(demoData) {
       .attr("y", d => yLinearScale(d.healthcare - 0.15))
       .text(d => d.abbr)
       .classed("stateText", true);
+    
+    // Create labels for circles // AGE
+    var age_abbr = chartGroup.selectAll(null)
+      .data(demoData)
+      .enter()
+      .append("text")
+      .attr("x", d => xLinearScale(d.age))
+      .attr("y", d => yLinearScale(d.healthcare))
+      .text(d => d.abbr)
+      .classed("stateText", true);
 
-    // Create axes labels
+    // Create group for two x-axis labels
+    var labelsGroup = chartGroup.append("g")
+        .attr("transform", `translate(${width / 2}, ${height + 20})`); 
+    
+    var povertyLabel = labelsGroup.append("text")
+      .attr("x", 0)
+      .attr("y", 20)
+      .attr("value", "poverty") // value to grab for event listener
+      .classed("active", true)
+      .text("Poverty Rate (%)");
+
+    var ageLabel = labelsGroup.append("text")
+      .attr("x", 0)
+      .attr("y", 40)
+      .attr("value", "age") // value to grab for event listener
+      .classed("inactive", true)
+      .text("Age (years)");
+
+  // // Creat x axis // ONE x axis
+  //   chartGroup.append("text")
+  //     .attr("transform", `translate(${width / 2.15}, ${height + margin.top + 30})`)
+  //     .attr("class", "axisText")
+  //     .text("Poverty Rate (%)");
+  // }).catch(function(error) {
+  //   console.log(error);
+    
+    // Create y axis labels
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left + 40,)
@@ -85,12 +159,59 @@ d3.csv("./assets/data/data.csv").then(function(demoData) {
       .attr("class", "axisText")
       .text("Residents Lacking Healthcare (%)");
 
-    chartGroup.append("text")
-      .attr("transform", `translate(${width / 2.15}, ${height + margin.top + 30})`)
-      .attr("class", "axisText")
-      .text("Poverty Rate (%)");
-  }).catch(function(error) {
-    console.log(error);
+  // x axis labels event listener
+  labelsGroup.selectAll("text")
+    .on("click", function() {
+    // get value of selection
+    var value = d3.select(this).attr("value");
+    if (value !== chosenXAxis) {
 
+      // replaces chosenXAxis with value
+      chosenXAxis = value;
+
+      // console.log(chosenXAxis)
+
+      // functions here found above csv import
+      // updates x scale for new data
+      xLinearScale = xScale(demoData, chosenXAxis);
+
+      // updates x axis with transition
+      xAxis = renderAxes(xLinearScale, xAxis);
+
+      // updates circles with new x values
+      circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
+
+      // changes classes to change bold text
+      if (chosenXAxis === "poverty") {
+        povertyLabel
+          .classed("active", true)
+          .classed("inactive", false);
+        ageLabel
+          .classed("active", false)
+          .classed("inactive", true);
+      pov_abbr
+        .classed("stateText", true);
+        
+      }
+      else {
+        povertyLabel
+          .classed("active", false)
+          .classed("inactive", true);
+        ageLabel
+          .classed("active", true)
+          .classed("inactive", false);
+        chartGroup.selectAll(null)
+          .data(demoData)
+          .enter()
+          .append("text")
+          .attr("x", d => xLinearScale(d.age))
+          .attr("y", d => yLinearScale(d.healthcare - 0.15))
+          .text(d => d.abbr)
+          .classed("stateText", true);
+      }
+    }
+  });
+}).catch(function(error) {
+console.log(error);
 
 })
